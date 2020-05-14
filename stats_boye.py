c5c1cd25f1f2c50af9c45ef0818bb22497e24519
Gabriel Chuang <gabrielchuang@gabriels-mbp.attlocal.net>
2020-05-13 17:32:46 -0700

    initial commit

---
+++ stats_boye.py
@@ -0,0 +1,32 @@
+import discord
+import time
+import sqlite3
+import datetime
+import pandas as pd 
+import numpy as np 
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import re
+
+token = open('token.txt').read()
+client = discord.Client()
+plt.close('all')
+
+def refresh_users(guild): 
+	..
+
+def refresh_channels(guild):
+	..
+
+def refresh_emoji(guild):
+	..
+
+def refresh_messages(guild):
+	..
+
+def send_query(query):
+	..
+
+
+
