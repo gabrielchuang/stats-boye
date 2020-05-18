eabd9a62afe9e5be06e6dfcc9784874316a9b9be
Gabriel Chuang <gabriel.t.chuang@gmail.com>
2020-05-18 01:57:35 -0700

    cleaned up admin commands

--- stats_boye.py
+++ stats_boye.py
@@ -13,19 +13,31 @@ token = open('token.txt').read()
 client = discord.Client()
 plt.close('all')
 
+cc = '!' # command character
+
 banned_channels = open('banned_channels.csv').read().split(',')
+admin_commands = ['add_ignored_channel', 'set_color', 'refresh_users', 'refresh_messages', 'clear_messages_table', 'refresh_channel']
+auth_admins = open('admins.csv').read().strip().split(',')
 
-print(banned_channels)
+print(auth_admins)
 
-
+# refresh_users : clears out the users db for the given guild and rebuilds it from scratch. 
+# 	returns True
 async def refresh_users(guild): 
 	conn = sqlite3.connect("information.db")
+	c = conn.cursor()
+	c.execute('DELETE FROM users WHERE guild_ID=?', (guild.id,))
+	conn.commit()
+	c.close()
+	mems = []
 	for member in guild.members:
-		c = conn.cursor()
-		c.execute('INSERT INTO users VALUES(?,?,?)', (member.id, member.name, ",".join([str(x.id) for x in member.roles])))
-		conn.commit()
-		c.close()
+		mems.append((member.id, member.name, ",".join([str(x.id) for x in member.roles]), guild.id))
+	c = conn.cursor()
+	c.executemany('INSERT INTO users VALUES(?,?,?,?)', mems)
+	conn.commit()
+	c.close()
 	conn.close()
+	return True
 
 # refresh_channels : goes through all channels of guild and updates their names, adds any 
 # 		new channels, etc. preserves color of existing channels. 
@@ -46,22 +58,25 @@ async def refresh_channels(guild):
 			conn.commit()
 			c.close()
 	conn.close()
+	return True
 
 # change_channel_color : changes the color associated with a given channel in the db. 
-async def change_channel_color(channel, new_color): 
+async def change_channel_color(channel, color): 
 	conn = sqlite3.connect("information.db")
 	c = conn.cursor()
 	c.execute('SELECT * FROM channels WHERE channel_ID=?', (channel.id,))
 	rows = c.fetchall()
 	if len(rows) == 0:
 		print("nonexistent channel or i can't access it")
+		return False 
 	else:
 		c.execute('DELETE FROM channels WHERE channel_ID=?', (channel.id,))
-		c.execute('INSERT INTO channels VALUES(?,?,?,?)', (channel.guild.id, channel.id, new_color, channel.name))
-		print("channel " + channel.name + "'s color has been updated to " + new_color)
+		c.execute('INSERT INTO channels VALUES(?,?,?,?)', (channel.guild.id, channel.id, color, channel.name))
+		print("channel " + channel.name + "'s color has been updated to " + color)
 		conn.commit()
 		c.close()
 	conn.close()
+	return True
 
 # add_banned_channel : adds a channel to the blacklist (stats bot will treat it like it 
 # 		does not exist). Deletes all records associated with the channel. 
@@ -83,9 +98,20 @@ async def add_banned_channel(channel):
 	banned_channels = open('banned_channels.csv').read().split(',')
 	return True
 
+async def add_admin(user):
+	global auth_admins
+	if str(user.id) in auth_admins:
+		return False
+	f = open('admins.csv', 'a')
+	f.write(','+str(user.id))
+	f.close()
+	auth_admins = open('admins.csv').read().split(',')
+	return True
+
 def refresh_emoji(guild):
 	pass
 
+# clear_all_entries : deletes all entries in `messages`. returns True. 
 async def clear_all_entries():
 	conn = sqlite3.connect("information.db")
 	c = conn.cursor()
@@ -93,7 +119,9 @@ async def clear_all_entries():
 	conn.commit()
 	c.close()
 	conn.close()
+	return True
 
+# get_most_recently_added : gets the most recent message in the channel. 
 async def get_most_recently_added(channel):
 	conn = sqlite3.connect("information.db")
 	c = conn.cursor()
@@ -104,7 +132,7 @@ async def get_most_recently_added(channel):
 	conn.close()
 	return last_id
 
-
+# refresh_messages : adds all new messages from the channel to the db. returns num msgs added.
 async def refresh_messages(channel):
 	last_message_here = await get_most_recently_added(channel)
 	
@@ -114,8 +142,8 @@ async def refresh_messages(channel):
 	message_data = []
 	count = 0
 	async for message in channel.history(limit=400000):
-		if count % 100 == 0:
-			print(count)
+		#if count % 100 == 0:
+		#	print(count)
 		if message.id == last_message_here:
 			break 
 
@@ -143,49 +171,60 @@ async def refresh_messages(channel):
 	print("added", count, "messages to db from channel", channel.name)
 	return count
 
+# refresh_all_messages : calls refresh_messages on each channel. 
+#	returns False if any fail, else True. 
 async def refresh_all_messages(guild): 
 	total_messages = 0
+	all_success = True
 	for channel in guild.channels:
 		if isinstance(channel, discord.channel.TextChannel) and str(channel.id) not in banned_channels:
 			try: 
 				total_messages = total_messages + await refresh_messages(channel)
 			except discord.errors.Forbidden:
 				print("exception raised on channel", channel.name)
+				all_success = False
 	print(total_messages, "messages added")
+	return all_success
 
-def send_query(query):
-	pass
+def parse_channels(message): #for now, only includes channel mentions. may extend to include not-pings and ID calls. 
+	return message.channel_mentions
 
-async def set_color(message):
-	channel = message.channel_mentions[0]
-	color = message.content.split()[2]
-	await change_channel_color(channel, color)
+async def run_admin_command(message):
+	await message.add_reaction('⚙️')
+	channels = parse_channels(message)
+
+	if cc+"add_ignored_channel" in message.content:
+		success = all([await add_banned_channel(x) for x in channels]) and len(channels) > 0
+	elif cc+"refresh_channels" in message.content:
+		success = await refresh_channels(message.guild)
+	elif cc+"set_color" in message.content: 
+		color = re.search('#[0-9a-fA-F]{6}', message.content).group(0)
+		success = all([await change_channel_color(x, color) for x in channels]) and len(channels) > 0
+	elif cc+"refresh_users" in message.content:
+		success = await refresh_users(message.guild)
+	elif cc+"refresh_channel" in message.content:
+		success = all([await refresh_messages(x) >= 0 for x in channels]) and len(channels) > 0
+	elif cc+"refresh_messages" in message.content:
+		success = await refresh_all_messages(message.guild)
+	else:
+		success = False
+
+	await message.remove_reaction('⚙️', message.guild.me)
+
+	if success:
+		await message.add_reaction("📈")
+	else: 
+		await message.add_reaction("❌")
+
 
 @client.event
 async def on_message(message): #currently very basic, just for testing (for now)
-	if "!add_ignored_channel" in message.content and "ezekiel" in str(message.author):
-		#add_ignored_channel [channel_1] [channel_2] 
-		for channel in message.channel_mentions:
-			if not await add_banned_channel(channel): 
-				await message.add_reaction('❌')
-		await message.add_reaction("📈")
-	if "!set_color" in message.content and "ezekiel" in str(message.author):
-		#message must be: !set_color [channel] #[color in hex]
-		await set_color(message)
-		await message.add_reaction("📈")
-	if "!refresh_users" in message.content and "ezekiel" in str(message.author):
-		await refresh_users(message.guild)
-		await message.add_reaction("📈")
-	if "!refresh_channel" in message.content and "ezekiel" in str(message.author):
-		await refresh_messages(message.channel_mentions[0])
-		await message.add_reaction("📈")
-	if "!clear_messages_table" in message.content and "ezekiel" in str(message.author):
-		await clear_all_entries()
-		await message.add_reaction("📈")
-	if "!refresh_messages" in message.content and "ezekiel" in str(message.author): 
-		await refresh_all_messages(message.guild)
-		await message.add_reaction("📈")
-
+	if any([(cc+x) in message.content for x in admin_commands]):
+		if str(message.author.id) in auth_admins:
+			await run_admin_command(message)
+		else:
+			# invalid perms!
+			await message.add_reaction("😡")
 
 @client.event
 async def on_ready():
